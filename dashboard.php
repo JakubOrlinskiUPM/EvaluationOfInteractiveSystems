f5e29986b28901b102e053c8c0a70950daa100f9
jakuborlinski <jakub.orlinski.upm@gmail.com>
2022-05-23 14:15:10 +0200

    Allow CORS requests

--- dashboard.php
+++ dashboard.php
@@ -13,6 +13,11 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, X-Requested-With');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
+header('Access-Control-Max-Age: 600');
+
 $query = "SELECT * FROM `ab_test`";
 
 if ($result = $conn->query($query)) {
